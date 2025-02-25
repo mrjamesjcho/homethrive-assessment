793469142f6463a04a9e42d25da7cc79039bb32f
James Cho <mrjamesjcho@gmail.com>
2025-02-25 02:07:34 -0800

    add basic auth

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,30 @@
 import fastify from "fastify";
 import routes from "./routes";
+import fastifyAuth from "@fastify/auth";
+import fastifyBasicAuth from "@fastify/basic-auth";
 import { NotFoundError } from "./errors";
 
 export const init = () => {
   const app = fastify();
+
+  // register basic auth
+  app.register(require("@fastify/auth"));
+  app.register(require("@fastify/basic-auth"), {
+    validate: async (username: string, password: string) => {
+      console.log("username", process.env.BASIC_AUTH_USERNAME);
+      if (
+        username !== process.env.BASIC_AUTH_USERNAME ||
+        password !== process.env.BASIC_AUTH_PASSWORD
+      ) {
+        return new Error("Unauthorized");
+      }
+    },
+  });
+  app.after(() => {
+    app.addHook("preHandler", app.auth([app.basicAuth]));
+  });
+
+  // register routes
   app.register(routes);
   app.setErrorHandler((error, request, reply) => {
     if (error instanceof NotFoundError) {
@@ -14,8 +35,8 @@ export const init = () => {
 };
 
 if (require.main === module) {
-  init().listen({ port: 3000 }, (err) => {
+  init().listen({ port: 3009 }, (err) => {
     if (err) console.error(err);
-    console.log("server listening on 3000");
+    console.log("server listening on 3009");
   });
 }
